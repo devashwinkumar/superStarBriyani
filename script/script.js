0d18cdb3e9e3380e57a547562a5cdc2151c02be0
ashwin2sr <ashwin2sr@gmail.com>
2020-03-17 21:10:35 +0800

    content update remaining

--- script/script.js
+++ script/script.js
@@ -7,58 +7,18 @@ $(document).ready(function() {
     $(".side-nav ").removeClass("open");
   });
 
-  var s = skrollr.init();
+  function resizeForm() {
+    var width =
+      window.innerWidth > 0
+        ? window.innerWidth
+        : document.documentElement.clientWidth;
+    if (width > 1100) var s = skrollr.init();
+  }
+  window.onresize = resizeForm;
+  resizeForm();
+
   $(".nav-item").click(function() {
     $("a.active").removeClass("active");
     $(this).addClass("active");
-    var active_section = $(this).attr("href"); //get active section id
-    $("html, body").animate(
-      {
-        //and scroll to the section
-        scrollTop: $(active_section).offset().top
-      },
-      800
-    );
-  });
-
-  $(document).scroll(function() {
-    //get document scroll position
-    var position = $(document).scrollTop();
-    //get header height
-    var header = $("nav").outerHeight();
-
-    //check active section
-    $(".section").each(function(i) {
-      if ($(this).position().top <= position) {
-        $("a.active").removeClass("active");
-        $("a")
-          .eq(i)
-          .addClass("active");
-      }
-    });
   });
 });
-
-// $("a").on("click", function(event) {
-//   // Make sure this.hash has a value before overriding default behavior
-//   if (this.hash !== "") {
-//     // Prevent default anchor click behavior
-//     event.preventDefault();
-
-//     // Store hash
-//     var hash = this.hash;
-
-//     // Using jQuery's animate() method to add smooth page scroll
-//     // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-//     $("html, body").animate(
-//       {
-//         scrollTop: $(hash).offset().top
-//       },
-//       800,
-//       function() {
-//         // Add hash (#) to URL when done scrolling (default click behavior)
-//         window.location.hash = hash;
-//       }
-//     );
-//   } // End if
-// });
